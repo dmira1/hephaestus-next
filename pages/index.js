05fd90e79cca3e20262ac22c540809ad6a3e524b
dmira1 <darryl.pmira@gmail.com>
2022-04-21 17:20:04 -0600

    Payment Checkout Complete

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,17 @@
 import PageTitle from "../components/PageTitle/PageTitle"
 import ProductCard from "../components/ProductCard/ProductCard";
 import Head from "next/head";
+import { loadStripe } from "@stripe/stripe-js";
 
 export default function Home(props) {
-  const products = props.products;
+  const products = props.products
+  const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY)
   return (
     <>
       <Head>
-      <meta charset="UTF-8"/>
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <title>Hephaestus PC</title>
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <title>Hephaestus PC</title>
       </Head>
       <PageTitle tagline="All-In-One PCs" title="Hephaestus PC"/>
       <main>
@@ -19,7 +20,6 @@ export default function Home(props) {
     </>
   )
 }
-
 
 export async function getStaticProps(){
   const res = await fetch('https://hephaestus-pc-default-rtdb.firebaseio.com/products.json')
